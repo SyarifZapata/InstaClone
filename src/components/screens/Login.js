ce47806ad695d7bcc51b4e12133ca2fd9b75df00
syarif <syarif.hidayatullah@stud.unibas.ch>
2019-02-02 17:28:48 +0100

    finished rest api

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -6,11 +6,22 @@ class Login extends Component {
       header: null
     };
 
+    loginFacebook(){
+        return fetch('http://139.162.182.153:3000/auth/test').then((result)=>{
+            return (JSON.parse(result._bodyText));
+        }).then((jsonob)=>{
+            alert(jsonob.message)
+        }).catch((error)=>{
+            console.log(error);
+        });
+    }
+
     render(){
         return (
 
             <TouchableOpacity style={{height:100+'%', width: 100+ '%', flex: 1, justifyContent: 'center', alignItems: 'center'}}
-            onPress={()=>this.props.navigation.navigate('main')}>
+            // onPress={()=>this.props.navigation.navigate('main')}
+                onPress={this.loginFacebook}>
                 <StatusBar backgroundColor='rgb(245,245,245)' barStyle='dark-content'/>
                 <Text>LOGIN PAGE</Text>
             </TouchableOpacity>
